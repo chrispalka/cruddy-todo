aca73909202c24923ae901e3545a7fb12228588f
Dominic Silvia <dominic.silvia@gmail.com>
2020-06-06 11:23:47 -0700

    Complete Bare Minimum Requirements

--- datastore/index.js
+++ datastore/index.js
@@ -5,21 +5,16 @@ const counter = require('./counter');
 var Promise = require('bluebird');
 Promise.promisifyAll(fs);
 
-
-var items = {};
-// **** ask about ^ ***
-
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
   var id = counter.getNextUniqueId((err, id) => {
     if (err) {
-      throw ('ERROR!');
+      callback(new Error('Error creating todo item'))
     } else {
-      items[id] = text;
       fs.writeFile(path.join(exports.dataDir, `${id}.txt`), text, (err) => {
         if (err) {
-          throw ('Error!');
+          callback(new Error('Error creating todo item'))
         } else {
           callback(null, { id, text });
         }
@@ -28,61 +23,22 @@ exports.create = (text, callback) => {
   });
 };
 
-/**** What we had initially, using Promise.all ****
 exports.readAll = (callback) => {
-
   fs.readdirAsync(exports.dataDir)
     .then(function (fileList) {
-      console.log('filelist:', fileList);
       var promises = [];
-      _.each(fileList, (files) => {
-        promises.push(fs.readFileAsync(path.join(exports.dataDir, files), 'utf8'));
-      });
+      _.each(fileList, (file) => {
+        promises.push(fs.readFileAsync(path.join(exports.dataDir, file), 'utf8').then(function(text) {
+          var id = file.slice(0, -4);
+          return {id: id, text: text}}))
+      })
       Promise.all(promises)
         .then(function (data) {
-          callback(data);
-          // console.log('DATA:', data);
-          // return { data, data };
+          callback(null, data);
         });
-
-    });
-};
-***************************************/
-
-// Using Promise.map
-exports.readAll = (callback) => {
-  fs.readdirAsync(exports.dataDir)
-    .then(function (fileList) {
-      console.log('filelist:', fileList);
-      var promises = [];
-      var data = Promise.map(fileList, function(files) {
-        var id = files.slice(0, -4);
-        console.log('ID!!!', id);
-        // [{ id: '00001', text: 'todo 1' }, { id: '00002', text: 'todo 2' }] <--- what current test is expecting, array of objects
-        return {'id': id, 'text': fs.readFileAsync(path.join(exports.dataDir, files), 'utf8')};
-      }).then(function(data) {
-        console.log('DATA!!!', data);
-        callback(null, data);
-      });
-    });
+    }).catch(e => console.log('Error: ', e))
 };
 
-/**** Original readAll without promises ****
-exports.readAll = (callback) => {
-  fs.readdir(exports.dataDir, (err, files) => {
-    if (err) {
-      throw ('ERROR!');
-    } else {
-      console.log('files: ', files);
-      var data = _.map(files, (filename) => {
-        text = id = filename.slice(0, -4);
-        return { id, text };
-      });
-      callback(null, data);
-    }
-  });
-};
-***************************************/
 
 exports.readOne = (id, callback) => {
   fs.readFile(path.join(exports.dataDir, `${id}.txt`), (err, data) => {
@@ -96,13 +52,11 @@ exports.readOne = (id, callback) => {
 };
 
 exports.update = (id, text, callback) => {
-  var item = items[id];
   fs.readFile(path.join(exports.dataDir, `${id}.txt`), (err, data) => {
     if (err) {
       callback(new Error(`No item with id: ${id}`));
     } else {
-      item = text;
-      fs.writeFile(path.join(exports.dataDir, `${id}.txt`), item, (err) => {
+      fs.writeFile(path.join(exports.dataDir, `${id}.txt`), text, (err) => {
         if (err) {
           throw ('ERROR!');
         } else {
@@ -115,12 +69,11 @@ exports.update = (id, text, callback) => {
 
 
 exports.delete = (id, callback) => {
-  var item = items[id];
   fs.unlink(path.join(exports.dataDir, `${id}.txt`), (err) => {
     if (err) {
       callback(new Error(`No item with id: ${id}`));
     } else {
-      callback(id);
+      callback(null, id);
     }
   });
 };
